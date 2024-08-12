64d600992dd8cc74e2afa2b20b7e391c70d1f213
sulakshana763 <sulakshana763@gmail.com>
2024-08-11 21:34:42 -0700

    one file added

---
+++ ForLoop.java
@@ -0,0 +1,21 @@
+package helloworld;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(var i=0; i<4; i++) {
+			System.out.println("Hello");			
+		}
+		
+		int num1 = 1;
+		int num2 = 2;
+		if(num1>num2) {
+			System.out.println("num1 is greater than num2");
+		}else {
+			System.out.println("num2 is greater than num1");
+		}
+
+	}
+
+}
